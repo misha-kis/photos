4e3ee46ab6431d53899967cae2cfbd19078d5834
Mikhail Kiselyov <kiselev.0353@gmail.com>
2026-01-31 18:22:07 +0300

    comments

--- crates/photos-domain/src/lib.rs
+++ crates/photos-domain/src/lib.rs
@@ -12,52 +12,6 @@ pub use image_features::{
 
 pub type ImageId = photos_core::Uuid;
 
-// impl ImageFormat {
-//     pub fn as_u8(&self) -> u8 {
-//         match self {
-//             ImageFormat::Jpeg => 0,
-//             ImageFormat::Png => 1,
-//             ImageFormat::Webp => 2,
-//         }
-//     }
-// }
-//
-// impl TryFrom<u8> for ImageFormat {
-//     type Error = DomainError;
-//
-//     fn try_from(value: u8) -> Result<Self, Self::Error> {
-//         match value {
-//             0 => Ok(Self::Jpeg),
-//             1 => Ok(Self::Png),
-//             2 => Ok(Self::Webp),
-//             _ => Err(DomainError::UnsupportedFormat),
-//         }
-//     }
-// }
-//
-// impl AsRef<str> for ImageFormat {
-//     fn as_ref(&self) -> &str {
-//         match self {
-//             ImageFormat::Jpeg => "jpeg",
-//             ImageFormat::Png => "png",
-//             ImageFormat::Webp => "webp",
-//         }
-//     }
-// }
-//
-// impl TryFrom<&str> for ImageFormat {
-//     type Error = DomainError;
-//
-//     fn try_from(value: &str) -> Result<Self, Self::Error> {
-//         match value.to_lowercase().as_ref() {
-//             "jpeg" | "jpg" => Ok(Self::Jpeg),
-//             "png" => Ok(Self::Png),
-//             "webp" => Ok(Self::Webp),
-//             _ => Err(DomainError::UnsupportedFormat),
-//         }
-//     }
-// }
-
 pub struct Dimensions {
     pub width: u32,
     pub height: u32,
