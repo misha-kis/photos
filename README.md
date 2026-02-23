478a17038f6d68a5ae14131e9bfd7f4d7cf2a7c6
Mikhail Kiselyov <kiselev.0353@gmail.com>
2026-02-23 13:53:06 +0300

    build-rs for cv crate

---
+++ README.md
@@ -0,0 +1,4 @@
+# Photos App \[work in progress 🏗️\]
+
+A desktop Photo Library app with Face detection and clustering
+
---
+++ crates/photos-infra-cv/build.rs
@@ -0,0 +1,16 @@
+use std::{fs, path::PathBuf};
+
+async fn download(model: &str) {
+    let url = format!("https://github.com/misha-kis/photos/releases/download/v0.0.0/{model}");
+    let out_dir = PathBuf::from("../../assets/models");
+    fs::create_dir_all(&out_dir).unwrap();
+    let model_path = out_dir.join(model);
+    let bytes = reqwest::get(url).await.unwrap().bytes().await.unwrap();
+    tokio::fs::write(model_path, bytes).await.unwrap();
+}
+
+#[tokio::main]
+async fn main() {
+    tokio::join!(download("facenet.onnx"), download("yolov12n-face.onnx"));
+    println!("cargo:rerun-if-changed=build.rs");
+}
